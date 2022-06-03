e59857af8b5fec0f43f65f57695cf6581e5cad18
desalvatoredaniel <desalvatoredaniel@gmail.com>
2022-06-03 01:29:19 -0400

    test commit

--- myfirstreactapp/src/sidebannerLeft.js
+++ myfirstreactapp/src/sidebannerLeft.js
@@ -3,7 +3,7 @@ function LeftSideBar() {
     return (
         <div className='leftSide'>
           <h1>left banner ads go header</h1>
-          <h2></h2>
+          <h2>test test test</h2>
         </div>
 
     );
